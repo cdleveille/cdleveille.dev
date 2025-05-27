8992b7dff18e9f1b225768222202e95e2e3dd941
Chris Leveille <cdleveille@gmail.com>
2025-05-27 14:23:30 -0400

    change port to 3001

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,8 @@ const server = serve({
 	development: process.env.NODE_ENV !== "production" && {
 		hmr: true,
 		console: true
-	}
+	},
+	port: 3001
 });
 
 console.log(`🚀 Server running at ${server.url}`);
